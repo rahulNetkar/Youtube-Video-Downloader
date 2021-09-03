63fb28e30a350aa5ba4c0b65a99f736f626fd855
Rahul Netkar <68227147+rahulNetkar@users.noreply.github.com>
2021-09-03 21:17:52 +0530

    2nd page built

--- ytdown/lib/Screens/downloader_class.dart
+++ ytdown/lib/Screens/downloader_class.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_youtube_downloader/flutter_youtube_downloader.dart';
 
-Future<void> downloadVideo(
-    String? youTubeLink, String? title, int? itag) async {
-  await FlutterYoutubeDownloader.downloadVideo(youTubeLink!, title!, itag);
+class Download {
+  Future<void> downloadVideo(
+      String? youTubeLink, String? title, int? itag) async {
+    await FlutterYoutubeDownloader.downloadVideo(youTubeLink!, title!, itag);
+  }
 }
--- ytdown/lib/Screens/paste_link.dart
+++ ytdown/lib/Screens/paste_link.dart
@@ -7,20 +7,95 @@ class PasteLink extends StatefulWidget {
 }
 
 class _PasteLinkState extends State<PasteLink> {
+  TextEditingController _textController = TextEditingController();
+  int _itag = 22;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(),
-            ListTile(),
-            ListTile(),
-            ListTile(),
-            ListTile(),
+            TextFormField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                labelText: "Paste link here",
+                icon: Icon(Icons.paste),
+              ),
+            ),
+            ListTile(
+              title: const Text('HD'),
+              leading: Radio<int>(
+                value: 22,
+                groupValue: _itag,
+                onChanged: (int? value) {
+                  setState(() {
+                    _itag = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Video only (for live wallpapers)[303]'),
+              leading: Radio<int>(
+                groupValue: _itag,
+                value: 303,
+                onChanged: (int? value) {
+                  setState(() {
+                    _itag = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Video only (for live wallpapers)[399]'),
+              leading: Radio<int>(
+                groupValue: _itag,
+                value: 399,
+                onChanged: (int? value) {
+                  setState(() {
+                    _itag = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Audio only'),
+              leading: Radio<int>(
+                value: 251,
+                groupValue: _itag,
+                onChanged: (int? value) {
+                  setState(() {
+                    _itag = value!;
+                  });
+                },
+              ),
+            ),
             ElevatedButton(
-              onPressed: null,
-              child: Container(),
+              onPressed: () {
+                if (_textController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("No link pasted")));
+                } else {
+                  Download().downloadVideo(
+                      _textController.text, "Downloaded video", _itag);
+                }
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red[600])),
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
+                width: double.infinity,
+                color: Colors.red[800],
+                child: const Text(
+                  'Download',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
             )
           ],
         ),
